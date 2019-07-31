f3e0c600b5536b893813d4c6653b1a8289df523b
Dominik Hlavac Duran <dhlavacd@redhat.com>
2019-07-31 15:56:02 +0200

    Updated comment model required attributes

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,6 +1,6 @@
 class Comment < ApplicationRecord
   belongs_to :headline
 
-  validates_presence_of :author, :up_vote, :down_vote
+  validates_presence_of :author, :up_vote, :down_vote, :text
 
 end
